8fac67c1eb2911b1f59ac5733f5ec8bd63340583
Grégory Joseph <greg.jos@gmail.com>
2021-08-12 18:52:47 +1000

    TMP more tests/wip

--- src/patterns.test.ts
+++ src/patterns.test.ts
@@ -20,12 +20,40 @@ describe("Transposing & repeating functions", () => {
     ).toEqual([{ x: 50, y: 50 }, { x: 55, y: 60 }, { x: 60, y: 70 }, { x: 65, y: 80 }, { x: 70, y: 90 }]);
   });
 
-  it.todo("repeats in a grid spaced by same amount on both axis")//, () => {
-    // startAt(2,2).onGrid(3,4,10)//.map(COLLECT)
-  // });
+  it.skip("repeats in a grid spaced by same amount on both axis", () => {
+    expect(
+      startAt(2, 2).onGrid(3, 4, 10).map(COLLECT)
+    ).toEqual([
+      { x: 2, y: 2 }, { x: 12, y: 2 }, { x: 22, y: 2 },
+      { x: 2, y: 12 }, { x: 12, y: 12 }, { x: 22, y: 12 },
+      { x: 2, y: 22 }, { x: 12, y: 22 }, { x: 22, y: 22 },
+      { x: 2, y: 32 }, { x: 12, y: 32 }, { x: 22, y: 32 }
+    ]);
+  });
+  it.skip("repeats in a grid spaced differently per axis", () => {
+    expect(
+      startAt(2, 2).onGrid(3, 4, 5, 10).map(COLLECT)
+    ).toEqual([
+      { x: 2, y: 2 }, { x: 7, y: 2 }, { x: 12, y: 2 },
+      { x: 2, y: 12 }, { x: 7, y: 12 }, { x: 12, y: 12 },
+      { x: 2, y: 22 }, { x: 7, y: 22 }, { x: 12, y: 22 },
+      { x: 2, y: 32 }, { x: 7, y: 32 }, { x: 12, y: 32 }
+    ]);
+  });
+  it.skip("repeats in a grid based on total-size", () => {
+    expect(
+      startAt(0, 0).onGridSize(3, 4, { x: 30, y: 45 }).map(COLLECT)
+    ).toEqual([
+      { x: 0, y: 0 }, { x: 15, y: 0 }, { x: 30, y: 0 },
+      { x: 0, y: 15 }, { x: 15, y: 15 }, { x: 30, y: 15 },
+      { x: 0, y: 30 }, { x: 15, y: 30 }, { x: 30, y: 30 },
+      { x: 0, y: 45 }, { x: 15, y: 45 }, { x: 30, y: 45 }
+    ]);
+  });
 
-  it.todo("repeats in a grid spaced differently per axis")//, () => {
-    // startAt(2, 2).onGrid(3, 4, 5, 10);//.map(COLLECT)
-  // });
+  it.todo("repeats on a polygon of S sides of length L")
+  // startAt(0, 0).onPolygon(3, 30).map(COLLECT);
 
+  it.todo("repeats N times on a circle of D diameter")
+  // startAt(0, 0).onCircle(3, 30).map(COLLECT);
 });
--- src/patterns.ts
+++ src/patterns.ts
@@ -36,57 +36,31 @@ export namespace Patterns {
       return new Grid(this, horizontalItems, verticalItems, gridSpacingX, gridSpacingY);
     }
 
+    /**
+     * Places points on vertices of a polygon of N sides.
+     */
+    onPolygon(sides: number, sideLength: number) {
+
+    }
+
+    /**
+     * Equivalent to #onPolygon but places points on an enclosing circles; sides length unknown to user.
+     */
+    onCircle(repeats: number, diameter: number) {
+
+    }
   }
 
-  abstract class Move {
+  abstract class Repetition {
     constructor(readonly initial: Initial) {
     }
 
     // supply next coordinates to Repetition
-    abstract from(coords: Coords): Coords ;
-
     // TODO instead keep state here and have peek() and poll()
-  }
+    // abstract from(coords: Coords): Coords ;
+    abstract hasMore(): boolean ;
 
-  class Transposition extends Move {
-    constructor(initial: Initial, readonly transposition: OptCoords) {
-      super(initial);
-    }
-
-    // TODO split out builder/factories from logic/business methods?
-    /**
-     * Repeats this move the given number of times; 1 will simply execute the
-     * operation at the initial coordinates.
-     */
-    times(count: number) {
-      return new Repetition(this, count);
-    }
-
-    // supply next coordinates to Repetition
-    from(coords: Coords): Coords {
-      return {
-        x: coords.x + (this.transposition.x || 0),
-        y: coords.y + (this.transposition.y || 0)
-      };
-    }
-  }
-
-  class Grid extends Move {
-    constructor(initial: Initial, readonly horizontalItems: number, readonly verticalItems: number, readonly gridSpacingX: number, readonly gridSpacingY: number) {
-      super(initial);
-    }
-
-    from(coords: Coords): Coords {
-      return { x: 0, y: 0 };
-    }
-  }
-
-  class Repetition {
-    private readonly initial: Initial;
-
-    constructor(readonly move: Transposition, readonly repeats?: number) {
-      this.initial = move.initial;
-    }
+    abstract poll(): Coords ;
 
     do(fun: (coords: Coords) => void): void {
       this.map(fun);
@@ -96,12 +70,77 @@ export namespace Patterns {
      * Similar to do, but returns the results of `fun` for further processing
      */
     map<T>(fun: (coords: Coords) => T) {
-      let coords = [this.initial.coords];
-      // TODO if repeat <1, repeat until out of screen
-      for (let i = 1; i < (this.repeats || 0); i++) {
-        coords.push(this.move.from(coords[i - 1]));
+      let coords = [];
+      while (this.hasMore()) {
+        coords.push(this.poll());
       }
+      // let coords = [this.initial.coords];
+      // // TODO if repeat <1, repeat until out of screen
+      // for (let i = 1; i < (this.repeats || 0); i++) {
+      //   coords.push(this.from(coords[i - 1]));
+      // }
       return coords.map(fun);
     }
   }
+
+  class Transposition extends Repetition {
+    private repeats: number = 0; // builder
+    private currentIdx = 0; // state
+    private currentCoords: Coords | undefined = undefined; // state
+    constructor(initial: Initial, readonly transposition: OptCoords) {
+      super(initial);
+    }
+
+    // TODO split out builder/factories from logic/business methods?
+    /**
+     * Repeats this move the given number of times; 1 will simply execute the
+     * operation at the initial coordinates.
+     */
+    times(repeats: number) {
+      this.repeats = repeats;
+      return this;
+    }
+
+    hasMore(): boolean {
+      return this.currentIdx < this.repeats;
+    }
+
+    poll(): Coords {
+      if (!this.currentCoords) {
+        this.currentCoords = this.initial.coords;
+      } else {
+        this.currentCoords = {
+          x: this.currentCoords.x + (this.transposition.x || 0),
+          y: this.currentCoords.y + (this.transposition.y || 0)
+        };
+      }
+      this.currentIdx++;
+      return this.currentCoords;
+    }
+
+    // let coords = [this.initial.coords];
+    // // TODO if repeat <1, repeat until out of screen
+    // for (let i = 1; i < (this.repeats || 0); i++) {
+    //   coords.push(this.from(coords[i - 1]));
+    // }
+  }
+
+  class Grid extends Repetition {
+    constructor(initial: Initial, readonly horizontalItems: number, readonly verticalItems: number, readonly gridSpacingX: number, readonly gridSpacingY: number) {
+      super(initial);
+    }
+
+    from(coords: Coords): Coords {
+      return { x: 0, y: 0 };
+    }
+
+    hasMore(): boolean {
+      return false;
+    }
+
+    poll(): Coords {
+      return { x: 0, y: 0 };
+    }
+  }
+
 }
