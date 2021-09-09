a9b73d1a19e0a5ba0e072c49f09c337538a764c9
Grégory Joseph <greg.jos@gmail.com>
2021-09-09 23:42:24 +1000

    Code style for volumes.stories.ts

--- src/stories/volumes.stories.ts
+++ src/stories/volumes.stories.ts
@@ -6,43 +6,48 @@ import { cone, cup, cylinder, DevelopedVolume } from "../volumes";
 export default {
   title: "Patternator/Volumes",
   args: {
-    debug: false,
+    drawDebugHelp: false,
   },
 } as Meta;
 
-const makeStory = (makeVolume: (args: Args) => DevelopedVolume<any>): Story => (args: Args) => {
-  useEffect(() => {
-    const dev = makeVolume(args);
-    const snap = args.getSnap();
-    const path = snap
-      .path(dev.developed.pathSpec)
-      .attr({ fill: "none", strokeWidth: 1, stroke: "#000" });
-    const debug1 = args.debug ? debug(snap, dev, "#943") : [];
-    snap.group(path, ...debug1).transform("t 200 50");
-  }, [args]);
+const makeStory =
+  (makeVolume: (args: Args) => DevelopedVolume<any>): Story =>
+  (args: Args) => {
+    useEffect(() => {
+      const dev = makeVolume(args);
+      const snap = args.getSnap();
+      const path = snap.path(dev.developed.pathSpec);
+      const debug = drawDebug(args.drawDebugHelp, snap, dev);
+      snap.group(path, debug).transform("t 200 50");
+    }, [args]);
 
-  // if void, storybook prints "undefined" -- which we could pbly address via the decorator, but if decorator
-  // doesn't invoke story(), well, its useEffect is never invoked either.
-  return "";
-};
+    // if void, storybook prints "undefined" -- which we could pbly address via the decorator, but if decorator
+    // doesn't invoke story(), well, its useEffect is never invoked either.
+    return "";
+  };
 
-export const Cylinder = makeStory(args=> cylinder(100, 180));
-export const Cone = makeStory(args=> cone(100, 180));
-export const Cup = makeStory(args=> cup(60, 100, 150));
+export const Cylinder = makeStory((args) => cylinder(100, 180));
+export const Cone = makeStory((args) => cone(100, 180));
+export const Cup = makeStory((args) => cup(60, 100, 150));
 
-function debug(s:Snap.Paper, surface: DevelopedVolume<any>, txtClr: string) {
-  return [
+function drawDebug(
+  actuallyDebug: boolean,
+  s: Snap.Paper,
+  surface: DevelopedVolume<any>
+) {
+  if (!actuallyDebug) {
+    // return an empty group, i.e do nothing
+    return s.group();
+  }
+  return s.group(
     s.text(-20, -20, surface.description).attr(textAttr),
     ...surface.poi.flatMap((poi) => {
-      console.log("poi:", poi);
       return [
-        s.circle(poi.x, poi.y, 5).attr({ stroke: "red" }),
-        s
-          .text(poi.x + 5, poi.y + 5, poi.description)
-          .attr({ ...textAttr, fill: txtClr }),
+        s.circle(poi.x, poi.y, 5).attr(redStroke),
+        s.text(poi.x + 5, poi.y + 5, poi.description).attr(redText),
       ];
-    }),
-  ];
+    })
+  );
 }
 
 const textAttr = {
@@ -52,3 +57,5 @@ const textAttr = {
   textAnchor: "left",
   alignmentBaseline: "hanging",
 };
+const redText = { ...textAttr, fill: "#943" };
+const redStroke = { stroke: "red" };
