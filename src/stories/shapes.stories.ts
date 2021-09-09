28641b6771c72406686d6efb648e26a87783ffbc
Grégory Joseph <greg.jos@gmail.com>
2021-09-10 00:02:28 +1000

    Codestyle for shapes.stories.ts

--- src/stories/shapes.stories.ts
+++ src/stories/shapes.stories.ts
@@ -11,20 +11,22 @@ export default {
   title: "Patternator/Shapes",
 } as Meta;
 
-const makeStory = (makeShape: (args: Args) => Shape): Story => (args: Args) => {
-  useEffect(() => {
-    const shape = makeShape(args);
-    args
-      .getSnap()
-      .path(shape.pathSpec)
-      .attr({ fill: "none", strokeWidth: 1, stroke: "#000" })
-      .transform("t 50 50");
-  }, [args]);
+const makeStory =
+  (makeShape: (args: Args) => Shape): Story =>
+  (args: Args) => {
+    useEffect(() => {
+      const shape = makeShape(args);
+      args
+        .getSnap()
+        .path(shape.pathSpec)
+        .attr({ fill: "none", strokeWidth: 1, stroke: "#000" })
+        .transform("t 50 50");
+    }, [args]);
 
-  // if void, storybook prints "undefined" -- which we could pbly address via the decorator, but if decorator
-  // doesn't invoke story(), well, its useEffect is never invoked either.
-  return "";
-};
+    // if void, storybook prints "undefined" -- which we could pbly address via the decorator, but if decorator
+    // doesn't invoke story(), well, its useEffect is never invoked either.
+    return "";
+  };
 
 export const Rectangle = makeStory(() => rectangle(50, 80));
 export const SquareUsingRectangle = makeStory(() => rectangle(50, 50));
