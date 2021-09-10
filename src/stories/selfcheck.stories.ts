431ad9d9f50a5d8b892253d0cf1df157aa6a87d6
Grégory Joseph <greg.jos@gmail.com>
2021-09-10 23:23:10 +1000

    Print coords in self-check story

--- src/stories/selfcheck.stories.ts
+++ src/stories/selfcheck.stories.ts
@@ -7,18 +7,42 @@ export default {
 } as Meta;
 
 export const ViewboxText = (args: Args) => {
-  const fill = { fill: "#666", stroke: "#000" };
   useEffect(() => {
     const snap: Snap.Paper = args.getSnap();
     const viewBox = snap.attr("viewBox");
     console.log("viewBox:", viewBox);
-    snap.rect(0, 0, 6, 6).attr(fill);
-    snap.rect(viewBox.width - 6, 0, 6, 6).attr(fill);
-    snap.rect(viewBox.width - 6, viewBox.height - 6, 6, 6).attr(fill);
-    snap.rect(0, viewBox.height - 6, 6, 6).attr(fill);
+    coordTxt(snap, 0, 0, "left", "top");
+    coordTxt(snap, viewBox.width, 0, "right", "top");
+    coordTxt(snap, viewBox.width, viewBox.height, "right", "bottom");
+    coordTxt(snap, 0, viewBox.height, "left", "bottom");
     snap.rect(viewBox.cx - 3, viewBox.cy - 3, 6, 6).attr(fill);
     snap.rect(0, 0, viewBox.width, viewBox.height);
   }, [args]);
 
   return "";
+};
+
+const coordTxt = (
+  snap: Snap.Paper,
+  x: number,
+  y: number,
+  halign: "left" | "right",
+  valign: "top" | "bottom"
+) => {
+  const textX = halign === "left" ? x + 6 : x - 6;
+  const boxX = halign === "left" ? x : x - 6;
+  const textY = valign === "top" ? y + 6 : y - 6;
+  const boxY = valign === "top" ? y : y - 6;
+  snap.rect(boxX, boxY, 6, 6).attr(fill);
+  snap.text(textX, textY, `(${x},${y})`).attr(textAttr(halign, valign));
+};
+const fill = { fill: "#666", stroke: "#000" };
+const textAttr = (halign: "left" | "right", valign: "top" | "bottom") => {
+  return {
+    stroke: "none",
+    fill: "#333",
+    fontFamily: "Spot Mono",
+    textAnchor: halign === "left" ? "start" : "end",
+    alignmentBaseline: valign === "top" ? "hanging" : "auto", // can't say i really understand the nuances here
+  };
 };
